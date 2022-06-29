fc1008817e2fc3b6aa8561ff361c0d27c9f34a4d
waizinhtunscm <scm.waizinhtun@gmail.com>
2022-06-29 13:43:39 +0630

    add bootstrap,bootstrap-vue,headerbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,20 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+  <div>
+    <header-bar></header-bar>
+    <b-container>
+      <b-row class="mt-5">
+        <router-view />
+      </b-row>
+    </b-container>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import HeaderBar from "@/components/HeaderBar.vue";
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  components: {
+    HeaderBar,
+  },
+};
+</script>
